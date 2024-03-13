6d90bfaf54ca700e30062db1ea0f0fb8b602a28b
LynLyn1100 <160612206+LynLyn1100@users.noreply.github.com>
2024-03-13 11:51:57 +0700

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
- from django.test import TestCase
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
